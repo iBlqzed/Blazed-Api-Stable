90278adf7b75c7e4a4c4c9387a73b60d62475ebc
Aexx66 <joseckjesusrc1488@gmail.com>
2022-12-02 11:31:12 -0500

    Added some features for scoreboard manager
    Added:
    -world.scoreboard.setObjectiveAtDisplaySlot()
    -world.scoreboard.clearObjectiveAtDisplaySlot()
    -ObjectiveSortOrder enum
    -type DisplaySlotId
    -class ScoreboardObjetiveDisplayOptions

--- scripts/Api/ScoreboardManager.d.ts
+++ scripts/Api/ScoreboardManager.d.ts
@@ -1,3 +1,5 @@
+import { DisplaySlotId, ScoreboardObjectiveDisplayOptions } from "./Types";
+
 export declare class ScoreboardManager {
     /**
      * Add a scoreboard objective
@@ -10,4 +12,15 @@ export declare class ScoreboardManager {
      * @param {string} id The objective id to remove
      */
     removeObjective(id: string): void;
+    /**
+     * Sets an objective into display slot with specified additional display setting
+     * @param {DisplaySlotId} displaySlotId
+     * @param {ScoreboardObjectiveDisplayOptions} objectiveDisplaySetting
+     */
+    setObjectiveAtDisplaySlot(displaySlotId: DisplaySlotId, objectiveDisplaySetting: ScoreboardObjectiveDisplayOptions): void;
+    /**
+     * Clears the objective that occupies a display slot
+     * @param {DisplaySlotId} displaySlotId
+     */
+    clearObjectiveAtDisplaySlot(displaySlotId: DisplaySlotId): void;
 }
--- scripts/Api/Types.js
+++ scripts/Api/Types.js
@@ -5,3 +5,16 @@ export var Gamemode;
     Gamemode[Gamemode["adventure"] = 2] = "adventure";
     Gamemode[Gamemode["spectator"] = 3] = "spectator";
 })(Gamemode || (Gamemode = {}));
+
+/**
+ * Contains additional options for how a scoreboard should be
+ * displayed within its display slot.
+ */
+ export class ScoreboardObjectiveDisplayOptions {
+    objective
+    sortOrder
+    constructor(objective, sortOrder) {
+        this.objective = objective
+        this.sortOrder = sortOrder
+    }
+}
